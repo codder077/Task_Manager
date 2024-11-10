708d6b2592a3ba1773301ff4cbf799238895376b
codder077 <120477792+codder077@users.noreply.github.com>
2024-11-10 11:39:14 +0530

    chnage in backend

--- backend/utils/config.ts
+++ backend/utils/config.ts
@@ -2,7 +2,7 @@ export const corsOptions = {
     origin: [
       "http://localhost:5174",
       "http://localhost:5173",
-      "https://task-management-frontend-gules-five.vercel.app"
+      "https://task-manager-inxw.vercel.app"
       
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
